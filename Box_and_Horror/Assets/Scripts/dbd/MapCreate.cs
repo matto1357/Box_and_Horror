c4781d2edf408bf991037b13c4e7434c3dc24331
まちゅだ <darby8928@gmail.com>
2019-11-12 17:59:38 +0900

    まっぷをつくるよ！まだ思い通りにはつくれないよ！

---
+++ Box_and_Horror/Assets/Scripts/dbd/MapCreate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapCreate : MonoBehaviour
+{
+    [SerializeField] TextAsset textAsset;
+    [SerializeField] GameObject mapObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //本来ここでtextAssetの読み込み。多分managerから
+        //csvの読み込み
+        StreamReader reader = new StreamReader((Application.dataPath + "/Resources/CSV/" + textAsset.name + ".csv"));
+        int cnt = 0;
+        while(reader.Peek() > -1)
+        {
+            string[] str = reader.ReadLine().Split(',');
+            for (int i = 0;i< str.Length;i++)
+            {
+                if (int.Parse(str[i]) != 0)
+                {
+                    InstanceFloor(new Vector3(i, 0,cnt));
+                }
+            }
+            cnt++;
+        }
+    }
+    
+    private void InstanceFloor(Vector3 pos)
+    {
+        Instantiate(mapObject, pos, Quaternion.identity, this.transform);
+    }
+}
---
+++ Box_and_Horror/Assets/Scripts/dbd/MapCreateEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreateEditor : MonoBehaviour
+{
+    int[,] mapGlid;
+    [SerializeField] int[] glidLength = new int[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapGlid = new int[glidLength[0], glidLength[1]];
+        for (int i = 0;i< glidLength[0];i++)
+        {
+            for (int j = 0;j<glidLength[1];j++)
+            {
+                if(i != 3 && j != 3)
+                {
+                    mapGlid[i, j] = 1;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("in");
+            for (int i = 0;i<glidLength[0];i++)
+            {
+                string str = "";
+                for (int j = 0;j<glidLength[1];j++)
+                {
+                    str += mapGlid[i, j].ToString() + ",";
+                }
+                str = str.Substring(0,str.Length-1); ;
+                GetComponent<CSVWritter>().WritingCSV(str);
+            }
+        }
+    }
+}
