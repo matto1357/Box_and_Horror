d0c2eea6845ff6a58d217870471f886c1842a39e
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-11-12 18:47:08 +0900

    展開図判定途中

---
+++ Box_and_Horror/Assets/Scripts/Hirokawa/DeploymentBoxManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeploymentBoxManager : SingletonMonoBehaviour<DeploymentBoxManager>
+{
+    private Vector2Int[] poses = { new Vector2Int(5, 5), new Vector2Int(5, 6), new Vector2Int(5, 7), new Vector2Int(6, 6), new Vector2Int(7, 6), new Vector2Int(8, 6) };
+
+    private void Start()
+    {
+        Surfaces surfaces = new Surfaces(poses);
+        Vector2Int[] hoge = surfaces.ConvertedPoses;
+        foreach(Vector2Int data in hoge)
+        {
+            Debug.Log(data.x + ":" + data.y);
+        }
+        Debug.Log("size=" + surfaces.Size.x + ":" + surfaces.Size.y);
+    }
+}
---
+++ Box_and_Horror/Assets/Scripts/Hirokawa/Surfaces.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 面の情報を入れる
+/// </summary>
+public class Surfaces
+{
+    private Vector2Int[] poses;
+    private Vector2Int[] convertedPoses;
+
+    public Vector2Int[] Poses
+    {
+        set
+        {
+            if(value.Length != 6)
+            {
+                Debug.LogError("posesの要素数が6ではありません");
+                return;
+            }
+            this.poses = value;
+        }
+    }
+    public Vector2Int[] ConvertedPoses
+    {
+        get
+        {
+            if(convertedPoses == null)
+            {
+                this.convertedPoses = ConvertPoses(this.poses);
+            }
+            return this.convertedPoses;
+        }
+    }
+    public Vector2Int Size { get; set; }
+    
+    private Vector2Int[] ConvertPoses(Vector2Int[] data)
+    {
+        Vector2Int[] result = new Vector2Int[6];
+        Vector2Int min = data[0];
+        Vector2Int max = data[0];
+        for(int i = 1; i < result.Length; i++)
+        {
+            Vector2Int pos = data[i];
+            if(min.x > pos.x)
+            {
+                min.x = pos.x;
+            }
+            else if(max.x < pos.x)
+            {
+                max.x = pos.x;
+            }
+
+            if (min.y > pos.y)
+            {
+                min.y = pos.y;
+            }
+            else if (max.y < pos.y)
+            {
+                max.y = pos.y;
+            }
+        }
+
+        Debug.Log("min x:" + min.x + ", y:" + min.y);
+        Debug.Log("max x:" + max.x + ", y:" + max.y);
+
+        for(int i = 0; i < result.Length; i++)
+        {
+            Vector2Int pos = data[i];
+            result[i] = new Vector2Int(pos.x - min.x, pos.y - min.y);
+        }
+
+        Size = new Vector2Int(max.x - min.x + 1, max.y - min.y + 1);
+
+        return result;
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Surfaces()
+    {
+
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="_poses">要素数は6で</param>
+    public Surfaces(Vector2Int[] _poses)
+    {
+        Poses = _poses;
+    }
+}
