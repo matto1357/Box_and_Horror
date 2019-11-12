179d66810fd7eb5a024bd145c03e50db27cf3650
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-11-12 16:38:50 +0900

    シングルトンとエディタでシーン移動出来るやつ

---
+++ Box_and_Horror/Assets/Editor/SceneShortCut.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public enum SceneName
+{
+    none = -1,
+}
+
+public class SceneShortCut : Editor
+{
+    private static string scenePath = Application.dataPath + "/Scenes/";
+
+    private static string GetScenePath(SceneName scene)
+    {
+        return scenePath + scene.ToString() + ".unity";
+    }
+
+/*
+    [MenuItem("SceneShortCuts/2D_NotesTest")]
+    static public void Open2D_NotesTestScene()
+    {
+        //セーブ確認
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            // シーン遷移
+            EditorSceneManager.OpenScene(GetScenePath(SceneName.NotesTest_2D));
+        }
+    }
+*/
+}
---
+++ Box_and_Horror/Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シングルトン
+/// </summary>
+/// <typeparam name="T"></typeparam>
+
+//マネージャー系は先に実行させるテスト
+//ダメだったら個別に付けよう
+[DefaultExecutionOrder(-1)]
+public class SingletonMonoBehaviour<T> : 
+    MonoBehaviour where T: MonoBehaviour
+{
+    public static T instance;
+
+    protected virtual void Awake()
+    {
+        if(instance == null)
+        {
+            //シングルトン化
+            instance = this as T;
+        }
+        else
+        {
+            //Destroy
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(instance == this)
+        {
+            instance = null;
+        }
+    }
+}
