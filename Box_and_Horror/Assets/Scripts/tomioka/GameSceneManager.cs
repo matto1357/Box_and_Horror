1a7fed230fc809c25b80051f06e8907f280ce087
tokyotech-hos <tokyotech-hosyu@4900.co.jp>
2019-11-12 18:11:16 +0900

    Sceneの追加とタイトルから飛ぶ部分は作った

---
+++ Box_and_Horror/Assets/Scripts/tomioka/GameSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void StartButtonClick()
+    {
+        //メインシーンへ飛ぶ
+        SceneManager.LoadScene("");
+    }
+}
