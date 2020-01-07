6be1b758582aa432f95c0fd790903d5b0e8dedb6
Jason <jstai887@hotmail.com>
2020-01-07 18:51:40 +0900

    SoundManager, Enemy 追加

---
+++ Box_and_Horror/Assets/Scripts/Jason/EnemySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+
+    public void SpawnEnemy1(Vector3 position) 
+    {
+        Instantiate(enemy1, position, Quaternion.identity);
+    }
+
+    public void SpawnEnemy2(Vector3 position) 
+    {
+        Instantiate(enemy2, position, Quaternion.identity);
+    }
+
+    public void SpawnEnemy3(Vector3 position) 
+    {
+        Instantiate(enemy3, position, Quaternion.identity);
+    }
+}
